e4b34ecde854771e0ef6394b3afcd7310c402e28
Hilton Meyer <development@hiltonmeyer.com>
2022-05-10 14:25:42 +0300

    create scrapper

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-const express = require('express')
+const express = require('express');
+const axios = require('axios');
 const app = express();
 
 const PORT = process.env.port || 3000;
+const website = 'https://www.wildflowers.co.il/english/plant.asp?ID=831';
+
+try {
+  axios(website).then((response) => {
+    const html = response.data;
+    console.log(html);
+  });
+} catch (error) {
+  console.log(error, error.message);
+}
 
 app.listen(PORT, () => {
-    console.log(`server is running on port:${PORT}`)
-})
+  console.log(`server is running on port:${PORT}`);
+});
